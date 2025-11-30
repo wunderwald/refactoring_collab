94723d66d22aa317394b1e01f1559164757fc7da
Moritz Wunderwald <moritzwunderwald@yahoo.de>
2025-11-30 16:58:17 +0100

    destroyed structure, added more

--- Main.R
+++ Main.R
@@ -1,13 +1,32 @@
 # install.packages("XLConnect")
 library(XLConnect)
 
-# reads IBI values from XLSX (one sheet, one column, no header)
-parse_ibi <- function(path) {
-  ibi <- readWorksheet(loadWorkbook(path),sheet=1, header=FALSE)
-  colnames(ibi) <- c("IBI")
-  return(ibi$IBI)
-}
+ibi <- readWorksheet(loadWorkbook("HRV_a.xlsx"),sheet=1, header=FALSE)
+colnames(ibi) <- c("IBI")
+ibi_a <- ibi$IBI
+ibi <- readWorksheet(loadWorkbook("HRV_b.xlsx"),sheet=1, header=FALSE)
+colnames(ibi) <- c("IBI")
+ibi_b <- ibi$IBI
+min_length <- min(length(ibi_a), length(ibi_b))
+ibi_a <- ibi_a[1:min_length]
+ibi_b <- ibi_b[1:min_length]
+maxibi_a <- max(ibi_a)
+maxibi_b <- max(ibi_b)
+minibi_a <- min(ibi_a)
+minibi_b <- min(ibi_b)
+ibirange_a <- maxibi_a - minibi_a
+ibirange_b <- maxibi_b - minibi_b
+ibi_a_norm <- (ibi_a - minibi_a)/ibirange_a
+ibi_b_norm <- (ibi_b - minibi_b)/ibirange_b
 
-# parse IBI data
-ibi_a <- parse_ibi("HRV_a.xlsx")
-ibi_b <- parse_ibi("HRV_b.xlsx")
+t <- 1:min(length(ibi_a_norm), length(ibi_b_norm))
+plot(t, ibi_a_norm[t], type = "l", col = "blue", lwd = 2)
+lines(t, ibi_b_norm[t], col = "red", lwd = 2)
+# TODO plots
+# boxplots for each
+# line graphs together
+# line graph for xcorr
+# scatter plot together
+
+# TODO analysis
+
