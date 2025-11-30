ec9526ca60f802c4cfc74c06ef0e08927847ff4b
Moritz Wunderwald <moritzwunderwald@yahoo.de>
2025-11-30 17:24:58 +0100

    moved functions to Analysis.R

---
+++ Analysis.R
@@ -0,0 +1,24 @@
+# install.packages("XLConnect")
+library(XLConnect)
+
+parse_xlsx <- function(path) {
+  ibi <- readWorksheet(loadWorkbook("HRV_a.xlsx"),sheet=1, header=FALSE)
+  colnames(ibi) <- c("IBI")
+  return(ibi$IBI)
+}
+
+trim <- function(signal, other_signal) {
+  min_length <- min(length(signal), length(other_signal))
+  return(signal[1:min_length])
+}
+
+normalise <- function(signal) {
+  min_sample <- min(signal)
+  max_sample <- max(sample)
+  range <- max_sample - min_sample
+  return((signal - min_sample) / range)
+}
+
+xcorr <- function(signal_a, signal_b) {
+  return(ccf(signal_a, signal_b, plot = FALSE))
+}
--- Main.R
+++ Main.R
@@ -1,24 +1,21 @@
-# install.packages("XLConnect")
-library(XLConnect)
+source("Analysis.R")
+source("Plot.R")
 
-ibi <- readWorksheet(loadWorkbook("HRV_a.xlsx"),sheet=1, header=FALSE)
-colnames(ibi) <- c("IBI")
-ibi_a <- ibi$IBI
-ibi <- readWorksheet(loadWorkbook("HRV_b.xlsx"),sheet=1, header=FALSE)
-colnames(ibi) <- c("IBI")
-ibi_b <- ibi$IBI
-min_length <- min(length(ibi_a), length(ibi_b))
-ibi_a <- ibi_a[1:min_length]
-ibi_b <- ibi_b[1:min_length]
-maxibi_a <- max(ibi_a)
-maxibi_b <- max(ibi_b)
-minibi_a <- min(ibi_a)
-minibi_b <- min(ibi_b)
-ibirange_a <- maxibi_a - minibi_a
-ibirange_b <- maxibi_b - minibi_b
-ibi_a_norm <- (ibi_a - minibi_a)/ibirange_a
-ibi_b_norm <- (ibi_b - minibi_b)/ibirange_b
-cross_corr <- ccf(ibi_a_norm, ibi_b_norm, plot = FALSE)
+# read data
+ibi_a <- parse_xlsx("HRV_a.xlsx")
+ibi_b <- parse_xlsx("HRV_b.xlsx")
+
+# trim both ibi vectors to length of the shorter one
+ibi_a <- trim(ibi_a, ibi_b)
+ibi_b <- trim(ibi_b, ibi_a)
+
+# normalise both signals to range 0,1
+ibi_a_norm <- normalise(ibi_a)
+ibi_b_norm <- normalise(ibi_b)
+
+# calculate cross-correlation
+ccf = xcorr(ibi_a_norm, ibi_b_norm)
+
 t <- 1:min(length(ibi_a_norm), length(ibi_b_norm))
 plot(t, ibi_a_norm[t], type = "l", lwd = 2, col = "red")
 t <- 1:min(length(ibi_a_norm), length(ibi_b_norm))
