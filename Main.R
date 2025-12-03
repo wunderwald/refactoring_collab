19578aca056dec079b74cba3a872f467489e2502
Moritz Wunderwald <moritzwunderwald@yahoo.de>
2025-12-03 11:19:43 +0100

    added plot functions

--- Main.R
+++ Main.R
@@ -26,6 +26,24 @@ hist(ibi_b_norm, col = "blue")
 # modularise
 
 
+line_graph <- function(signal, color) {
+  t <- 1:length(signal)
+  plot(t, signal[t], type = "l", lwd = 2, col = color)
+}
+ibi_plots <- function(ibi, color) {
+  line_graph(ibi, color)
+  hist(ibi, col = color)
+  boxplot(ibi, col = color)
+}
+dyad_plot <- function(ibi_a, ibi_b, color) {
+  plot(ibi_a, ibi_b, pch = 16, col = color)
+}
+xcorr_plot <- function(xcorr_data, color) {
+  plot(xcorr_data, col = color)
+}
+
+
+
 ibi_a <- parse_xlsx("HRV_a.xlsx")
 ibi_b <- parse_xlsx("HRV_b.xlsx")
 ibi_a <- trim(ibi_a, ibi_b)
